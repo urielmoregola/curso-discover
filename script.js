415081af226eea041094b00708bf8a85ce716675
“uriel” <uriel.ninive@gmail.com>
2025-05-31 01:11:00 -0300

    add corrections script

--- script.js
+++ script.js
@@ -1,24 +1,16 @@
 function toggleMode() {
   const html = document.documentElement
-  /*if (html.classList.contains('light')) {
-  html.classList.remove('light')
+  html.classList.toggle("light")
+
+  //pegar a imagem
+  const img = document.querySelector("#profile img")
+
+  //substituir a imagem
+  if (html.classList.contains("light")) {
+    //se tiver lightMode adcionar a imagem
+    img.setAttribute("src", "./assets/avatar-light.png")
   } else {
-  html.classList.add('light')
-  }*/
- 
-html.classList.toggle('light')
-
-//pegar a imagem
-const img = document.querySelector('#profile img')
-
-//substituir a imagem
-if (html.classList.contains('light')) {
-  //se tiver lightMode adcionar a imagem
-  img.setAttribute("src", "./assets/avatar-light.png")
-} else {
-  //se tiver sem lightMode, mater a imagem padrão
-  img.setAttribute("src", "./assets/avatar.png")
+    //se tiver sem lightMode, mater a imagem padrão
+    img.setAttribute("src", "./assets/avatar.png")
+  }
 }
-}
-
-
